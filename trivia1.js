dca36df1f9f48fa6ca48b9e46dfa2c1a65dc8a9d
archana-nashik <151771398+archana-nashik@users.noreply.github.com>
2023-11-23 18:34:29 +0530

    Update trivia1.js

--- trivia1.js
+++ trivia1.js
@@ -1,42 +1,12 @@
-const questions = [
-  {
-    question: "Which country has the largest population in the world?",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_China_%28PRC%29.svg/220px-Flag_of_China_%28PRC%29.svg.png",
-    options: [
-      { text: "China", isCorrect: true },
-      { text: "India", isCorrect: false },
-      { text: "United States", isCorrect: false },
-      { text: "Indonesia", isCorrect: false },
-    ],
-  },
-  {
-    question: "What is the chemical symbol for silver?",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/100_gram_silver_bar.jpg/220px-100_gram_silver_bar.jpg",
-    options: [
-      { text: "Au", isCorrect: false },
-      { text: "Ag", isCorrect: true },
-      { text: "Cu", isCorrect: false },
-      { text: "Pt", isCorrect: false },
-    ],
-  },
-  {
-    question: "What is the capital of Italy?",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Rome_Colosseum_%28from_above%29.jpg/220px-Rome_Colosseum_%28from_above%29.jpg",
-    options: [
-      { text: "Rome", isCorrect: true },
-      { text: "Milan", isCorrect: false },
-      { text: "Florence", isCorrect: false },
-      { text: "Naples", isCorrect: false },
-    ],
-  },
-  {
-    question: "Which planet is known as the 'Red Planet'?",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Mars_from_Space_%28PIA041006%29.jpg/220px-Mars_from_Space_%28PIA041006%29.jpg",
-    options: [
-      { text: "Mercury", isCorrect: false },
-      { text: "Venus", isCorrect: false },
-      { text: "Earth", isCorrect: false },
-      { text: "Mars", isCorrect: true },
-    ],
-  },
-];
+function checkAnswer(answer) {
+    const correctAnswer = "Paris";
+    const feedbackElement = document.getElementById("feedback");
+
+    if (answer === correctAnswer) {
+        feedbackElement.textContent = "Correct!";
+        feedbackElement.classList.add("correct");
+    } else {
+        feedbackElement.textContent = "Incorrect. The correct answer is " + correctAnswer;
+        feedbackElement.classList.add("incorrect");
+    }
+}
